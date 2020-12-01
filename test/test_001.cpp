c27324d82d709c07c0ae60437d3a812b188a863c
rob tillaart <rob.tillaart@gmail.com>
2020-12-01 20:05:10 +0100

    more serious test

--- test/test_001.cpp
+++ test/test_001.cpp
@@ -2,18 +2,18 @@
 #include <ArduinoUnitTests.h>
 #include "SHT31.h"
 
-SHT31 sht;
+SHT31 sht_global;
 
 unittest(test_constructor)
 {
   SHT31 sht;
-  assertNotEqual(sht, NULL);
+  assertNotEqual(sht, sht_global);
 }
 
 unittest(test_begin)
 {
   SHT31 sht;
-  assertNotEqual(sht, NULL);
+  assertNotEqual(sht, sht_global);
 
   bool b = sht.begin(0x44);
   assertEqual(b, true);
